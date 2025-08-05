1f0869f975af114fc2888d6d985f1882632c3877
daniilshevch <daniil.shevchenko.shi.2023@lpnu.ua>
2025-08-05 23:30:36 +0300

    Added authentication info to navbar

--- railwayfrontend/src/components/Navbar.jsx
+++ railwayfrontend/src/components/Navbar.jsx
@@ -1,7 +1,21 @@
 ﻿import { Link } from 'react-router-dom';
 import "./Navbar.css";
+import {getCurrentUser} from "../utils/UserDefiner.js";
+import { Dropdown, Button, Menu } from 'antd';
+import { DownOutlined, UserOutlined, IdcardOutlined, LoginOutlined } from '@ant-design/icons';
+import {useEffect, useState} from "react";
 function Navbar()
 {
+    const [user, setUser] = useState(null);
+    useEffect(() => {
+        const currentUser = getCurrentUser();
+        setUser(currentUser);
+    }, [])
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        window.location.reload();
+    }
+
     return (
         <header className="navbar-wrapper">
             <img src="/background_images/images.png" alt="railway-logo" className="logo-image" />
@@ -12,9 +26,42 @@ function Navbar()
                     <li><Link className="nav-button" to="/">РОЗКЛАД РУХУ</Link></li>
                 </ul>
             </nav>
-            <Link to="/profile" className="profile-icon">
-                <img src = "/test.png" alt = "profile" />
-            </Link>
+            <div className="profile-area">
+                {user ? (
+                    <Dropdown overlayClassName="custom-dropdown-menu" menu={{
+                        items: [
+                            {
+                                key: "profile",
+                                icon: <UserOutlined />,
+                                label: <Link to="/profile">Профіль</Link>,
+                            },
+                            {
+                                key: "tickets",
+                                icon: <IdcardOutlined />,
+                                label: <Link to="/profile">Мої квитки</Link>,
+                            },
+                            {
+                                key: "logout",
+                                icon: <LoginOutlined />,
+                                label: "Вийти",
+                                onClick: handleLogout
+                            },
+                        ],
+                    }}>
+                        <Button className="account-button-authenticated" type="default">
+                            {user.name} <DownOutlined />
+                        </Button>
+                    </Dropdown>
+                ) : (
+                    <Link to = "/login">
+                        <Button className="account-button-not-authenticated" type="primary">УВІЙТИ</Button>
+                    </Link>
+                )}
+                <div className="profile-icon">
+                    <img src = "/test.png" alt = "profile" />
+                </div>
+            </div>
+
         </header>
     );
 }
---
+++ railwayfrontend/src/utils/UserDefiner.js
@@ -0,0 +1,18 @@
+﻿import {jwtDecode} from 'jwt-decode';
+export function getCurrentUser()
+{
+    const token = localStorage.getItem('token');
+    if(!token) return null;
+    try {
+        const decoded = jwtDecode(token);
+        return {
+            id: decoded["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier"],
+            name: decoded["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name"],
+            email: decoded["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress"]
+        };
+    }
+    catch
+    {
+        return null;
+    }
+}
