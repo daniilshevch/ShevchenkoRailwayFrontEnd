eee432710f1ec7f2503dbcce11fdfae1e955d8ec
daniilshevch <daniil.shevchenko.shi.2023@lpnu.ua>
2025-08-23 21:38:05 +0300

    Refactoring continued

--- railwayfrontend/src/AdminPanel/components/AdminTrainRoutesList/AdminTrainRoutesList.jsx
+++ railwayfrontend/src/AdminPanel/components/AdminTrainRoutesList/AdminTrainRoutesList.jsx
@@ -13,7 +13,9 @@ import {
 } from 'antd';
 import {enumOptions, enumOptionsForStrings} from "../GeneralComponents/EnumOptionConvertion.jsx"
 import {SPEED_TYPE_OPTIONS, TRIP_TYPE_OPTIONS, FREQUENCY_TYPE_OPTIONS,
-    ASSIGNMENT_TYPE_OPTIONS, TRAIN_QUALITY_CLASS_OPTIONS, RAILWAY_BRANCH_OPTIONS} from "./AdminTrainRoutesEnums.js"
+    ASSIGNMENT_TYPE_OPTIONS, TRAIN_QUALITY_CLASS_OPTIONS, RAILWAY_BRANCH_OPTIONS} from "./AdminTrainRoutesEnums.js";
+import AdminTrainRoutesTable from "./AdminTrainRoutesTable.jsx";
+
 import './AdminTrainRoutesList.css';
 
 const { Option } = Select;
@@ -24,51 +26,19 @@ const { Option } = Select;
 function AdminTrainRoutesList() {
     const [routes, setRoutes] = useState([]);
     const [createForm] = Form.useForm();
-    const [updateForm] = Form.useForm();
-    const [editingKey, setEditingKey] = useState('');
     const [isCreateModalVisible, setIsCreateModalVisible] = useState(false);
-    const navigate = useNavigate();
-
+    
     const fetchRoutes = async () => {
         const res = await fetch('https://localhost:7230/Admin-API/get-train-routes');
         const data = await res.json();
         setRoutes(data);
     };
-
     useEffect(() => {
         fetchRoutes();
     }, []);
 
-    const isEdited = (record) => record.id === editingKey;
 
-    const update = (record) => {
-        updateForm.setFieldsValue({ ...record });
-        setEditingKey(record.id);
-    };
 
-    const cancelUpdate = () => setEditingKey('');
-
-    const saveUpdate = async (id) => {
-        try {
-            const row = await updateForm.validateFields();
-            const updatedRoute = { ...routes.find((r) => r.id === id), ...row };
-
-            const response = await fetch(`https://localhost:7230/Admin-API/update-train-route/${id}`, {
-                method: 'PUT',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(updatedRoute),
-            });
-
-            if (!response.ok) throw new Error('Помилка при оновленні');
-
-            message.success('Оновлено');
-            setEditingKey('');
-            fetchRoutes();
-        } catch (err) {
-            console.error(err);
-            message.error('Помилка при збереженні');
-        }
-    };
 
     const handleCreate = async () => {
         try {
@@ -90,234 +60,15 @@ function AdminTrainRoutesList() {
             message.error('Не вдалося створити маршрут');
         }
     };
-    const handleDelete = async (id) =>
-    {
-        try
-        {
-            const response = await fetch(`https://localhost:7230/Admin-API/delete-train-route/${id}`, {
-                method: 'DELETE'
-            });
-            if (!response.ok) throw new Error("Помилка при видаленні");
-            fetchRoutes();
-        }
-        catch (err)
-        {
-            console.error(err);
-            message.error("Не вдалося видалити маршрут");
-        }
-    }
-    const showTrainRaces = async (train_route_id) =>
-    {
-        navigate(`/admin/${train_route_id}/train-races-list`);
-    }
 
-    const columns = [
-        {
-            title: 'ID',
-            dataIndex: 'id',
-            width: 50,
-        },
-        {
-            title: 'Фірмова назва',
-            dataIndex: 'branded_name',
-            editable: true,
-            render: (_, record) =>
-                isEdited(record) ? (
-                    <Form.Item name="branded_name" style={{ margin: 0 }}>
-                        <Input />
-                    </Form.Item>
-                ) : (
-                    record.branded_name
-                ),
-        },
-        {
-            title: 'Філія',
-            dataIndex: 'railway_branch_title',
-            editable: true,
-            render: (_, record) =>
-                isEdited(record) ? (
-                    <Form.Item name="railway_branch_title" style={{ margin: 0 }}>
-                        <Select>{enumOptionsForStrings(RAILWAY_BRANCH_OPTIONS)}</Select>
-                    </Form.Item>
-                ) : (
-                    RAILWAY_BRANCH_OPTIONS[record.railway_branch_title]
-                ),
-            width: 50,
-        },
-        {
-            title: 'Клас',
-            dataIndex: 'quality_class',
-            editable: true,
-            render: (_, record) =>
-                isEdited(record) ? (
-                    <Form.Item name="quality_class" style={{ margin: 0 }}>
-                        <Select>{enumOptions(TRAIN_QUALITY_CLASS_OPTIONS)}</Select>
-                    </Form.Item>
-                ) : (
-                    TRAIN_QUALITY_CLASS_OPTIONS[record.quality_class]
-                ),
-        },
-        {
-            title: 'Коефіцієнт маршруту',
-            dataIndex: 'train_route_coefficient',
-            editable: true,
-            render: (_, record) =>
-                isEdited(record) ? (
-                    <Form.Item name="train_route_coefficient" style={{ margin: 0 }}>
-                        <Input />
-                    </Form.Item>
-                ) : (
-                    record.train_route_coefficient
-                ),
-            width: 50,
-        },
-        {
-            title: 'Тип подорожі',
-            dataIndex: 'trip_type',
-            editable: true,
-            render: (_, record) =>
-                isEdited(record) ? (
-                    <Form.Item name="trip_type" style={{ margin: 0 }}>
-                        <Select>{enumOptions(TRIP_TYPE_OPTIONS)}</Select>
-                    </Form.Item>
-                ) : (
-                    TRIP_TYPE_OPTIONS[record.trip_type]
-                ),
-            width: 100,
-        },
-        {
-            title: 'Швидкісний тип',
-            dataIndex: 'speed_type',
-            editable: true,
-            render: (_, record) =>
-                isEdited(record) ? (
-                    <Form.Item name="speed_type" style={{ margin: 0 }}>
-                        <Select>{enumOptions(SPEED_TYPE_OPTIONS)}</Select>
-                    </Form.Item>
-                ) : (
-                    SPEED_TYPE_OPTIONS[record.speed_type]
-                ),
-            width: 100,
-        },
-        {
-            title: 'Частота курсування',
-            dataIndex: 'frequency_type',
-            editable: true,
-            render: (_, record) =>
-                isEdited(record) ? (
-                    <Form.Item name="frequency_type" style={{ margin: 0 }}>
-                        <Select>{enumOptions(FREQUENCY_TYPE_OPTIONS)}</Select>
-                    </Form.Item>
-                ) : (
-                    FREQUENCY_TYPE_OPTIONS[record.frequency_type]
-                ),
-            width: 100,
-        },
-        {
-            title: 'Тип призначення',
-            dataIndex: 'assignment_type',
-            editable: true,
-            render: (_, record) =>
-                isEdited(record) ? (
-                    <Form.Item name="assignment_type" style={{ margin: 0 }}>
-                        <Select>{enumOptions(ASSIGNMENT_TYPE_OPTIONS)}</Select>
-                    </Form.Item>
-                ) : (
-                    ASSIGNMENT_TYPE_OPTIONS[record.assignment_type]
-                ),
-            width: 100,
-        },
-        {
-            title: 'Фірмовий',
-            dataIndex: 'is_branded',
-            render: (_, record) =>
-                isEdited(record) ? (
-                    <Form.Item name="is_branded" valuePropName="checked" style={{ margin: 0 }}>
-                        <Switch />
-                    </Form.Item>
-                ) : record.is_branded ? (
-                    '✅'
-                ) : (
-                    '❌'
-                ),
-        },
-
-        {
-            title: 'Рейси',
-            dataIndex: 'train_races',
-            render: (_, record) => (
-                    <Button onClick={() => showTrainRaces(record.id)} type="link">
-                        Переглянути рейси
-                    </Button>
-            ),
-            width: 10,
-        },
-        {
-            title: 'Дії',
-            dataIndex: 'actions',
-            render: (_, record) =>
-                isEdited(record) ? (
-                    <>
-                        <Popconfirm
-                            title="Підтвердити збереження?"
-                            onConfirm={() => saveUpdate(record.id)}
-                            okText="Так"
-                            cancelText="Ні"
-                        >
-                            <Button type="link">Зберегти</Button>
-                        </Popconfirm>
-                        <Popconfirm
-                            title="Скасувати зміни?"
-                            onConfirm={cancelUpdate}
-                            okText="Так"
-                            cancelText="Ні"
-                        >
-                            <Button type="link">Скасувати</Button>
-                        </Popconfirm>
-                    </>
-                ) : (
-                    <>
-                        <Button disabled={editingKey !== ''} onClick={() => update(record)} type="link">
-                            Редагувати
-                        </Button>
-                        <Popconfirm
-                            title="Ви впевнені, що хочете видалити цей маршрут?"
-                            onConfirm={() => handleDelete(record.id)}
-                            okText="Так"
-                            cancelText="Ні"
-                        >
-                            <Button type="link" danger>
-                                Видалити
-                            </Button>
-                        </Popconfirm>
-                    </>
-                ),
-        },
-    ];
-
-    return (
+  return (
         <>
         <div className = "create-button-wrapper">
             <Button type="primary" onClick={() => setIsCreateModalVisible(true)} style={{ marginBottom: 16 }}>
                 + Додати маршрут
             </Button>
         </div>
-
-            <Form form={updateForm} component={false}>
-                <Table
-                    dataSource={routes}
-                    columns={columns}
-                    rowKey="id"
-                    bordered
-                    pagination={false}
-                    components={{
-                        body: {
-                            cell: ({ children }) => <td>{children}</td>,
-                        },
-                    }}
-                    rowClassName={(_, index) => (index % 2 === 0 ? 'light-blue-row' : 'dark-blue-row')}
-                    />
-            </Form>
+            <AdminTrainRoutesTable routes={routes} fetchRoutes={fetchRoutes}  />
 
             <Modal
                 title="Новий маршрут"
